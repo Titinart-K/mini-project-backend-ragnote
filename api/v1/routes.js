b1294c97857403641780000e931984942bf22d7b
titinart <k.titinart@gmail.com>
2025-09-04 14:29:37 +0700

    npm audit fix

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -6,8 +6,8 @@ import mongoNotes from "./mongo/notes.js";
 
 export default (db = 0) => {
   const router = express.Router();
-  // router.use(userRoutes(db));
-  // router.use(noteRoutes(db));
+  router.use(userRoutes(db));
+  router.use(noteRoutes(db));
   router.use("/mongo", mongoUsers);
   router.use("/mongo", mongoNotes);
   return router;
